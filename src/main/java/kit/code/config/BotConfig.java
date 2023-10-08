794d739702c907367ccb0f2bfd42cadf708d70b0
Dima <45449261+kitdim@users.noreply.github.com>
2023-10-08 17:13:50 +0300

    Delete src/main/java/kit/code directory

--- src/main/java/kit/code/config/BotConfig.java
+++
@@ -1,6 +0,0 @@
-package kit.code.config;
-
-public class BotConfig {
-    public static final String BOT_NAME = "name_your_bot";
-    public static final String TOKEN = "token_your_bot";
-}
--- src/main/java/kit/code/config/ItSupportConfig.java
+++
@@ -1,5 +0,0 @@
-package kit.code.config;
-
-public class ItSupportConfig {
-    public static final String ID_GROUP = "id_your_group";
-}
